839027b79e487f86da8fcdc7d2e4de0269a6b7a0
AnjaArisoa <119120342+AnjaArisoa@users.noreply.github.com>
2023-04-14 11:43:58 +0300

    sprint3

--- framework/FrontServlet.java
+++ framework/FrontServlet.java
@@ -1,22 +1,55 @@
 package etu2007.framework.servlet;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import etu2007.annotation.Url;
+import etu2007.framework.Mapping;
+import etu2007.framework.ModelView;
+import utils.PackageTool;
 import java.io.*;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 
 public class FrontServlet extends HttpServlet{
-    HashMap<String,Mapping> MappingUrls;
-    
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
-        res.setContentType("text/plain");
-        PrintWriter out = res.getWriter();
-        out.println(req.getRequestURL());
-    }
-    
-    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
-        res.setContentType("text/plain");
-        PrintWriter out = res.getWriter();
-        out.println(req.getRequestURL());
+    HashMap<String,Mapping> urlMapping = new HashMap<>();
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            for (Class c : PackageTool.inPackage(getServletConfig().getInitParameter("model"))){
+                for (Method m : c.getDeclaredMethods()){
+                    if(m.isAnnotationPresent(Url.class)){
+                        urlMapping.put(m.getAnnotation(Url.class).url(), new Mapping(c.getName(), m.getAnnotation(Url.class).url().split("-")[1]));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    protected void processRequest(HttpServletRequest req,HttpServletResponse res) throws IOException{
+        res.setContentType("text/plain");
+        PrintWriter out = res.getWriter();
+        String url = req.getRequestURI();
+        url = url.split("/")[url.split("/").length - 1];
+        out.println(url);
+        if(urlMapping.containsKey(url)){
+            try {
+                Object act = Class.forName(urlMapping.get(url).getClassName()).newInstance();
+                ModelView mv = (ModelView)act.getClass().getDeclaredMethod(urlMapping.get(url).getMethod()).invoke(act);
+                
+                RequestDispatcher requestDispatcher = req.getRequestDispatcher(mv.getView());    
+                requestDispatcher.forward(req,res);
+            } catch (Exception e) {
+                    e.printStackTrace();
+            }
+        }
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+        processRequest(req, res);
+    }
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+        processRequest(req, res);
+    }
 }
