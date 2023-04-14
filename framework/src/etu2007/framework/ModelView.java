561469015e251db7fe44f7e3674e820b76d2fa66
AnjaArisoa <119120342+AnjaArisoa@users.noreply.github.com>
2023-04-14 11:56:36 +0300

    sprint5

---
+++ framework/src/etu2007/framework/ModelView.java
@@ -0,0 +1,28 @@
+package etu2007.framework;
+import java.util.HashMap;
+
+public class ModelView {
+    String view;
+    HashMap<String,Object> data=new HashMap<>();
+
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+    public HashMap<String,Object> getData(){
+        return data;
+    }
+    public void setData(HashMap<String,Object> data){
+        this.data=data;
+    } 
+    public void addItem(String key,Object value){
+        data.put(key,value);
+    }
+}
+
+
+
+
