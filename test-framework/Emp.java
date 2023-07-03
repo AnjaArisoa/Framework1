821fba48389fe5b5958857ba648d2bd79b6bf965
AnjaArisoa <119120342+AnjaArisoa@users.noreply.github.com>
2023-07-03 11:02:53 +0300

    Update Emp.java

--- test-framework/Emp.java
+++ test-framework/Emp.java
@@ -1,12 +1,22 @@
 package model;
 import etu2007.annotation.Url;
 import etu2007.framework.ModelView;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 
 public class Emp {
     @Url(url="emp-insert")
     public ModelView insert(){
         ModelView mv = new ModelView();
         mv.setView("../emplist.jsp");
+        List<String> list = new ArrayList<>();
+        ne.add("Test1");
+        ne.add("Test2");
+        HashMap<String,Object> hash = new HashMap<>();
+        hash.put("lst", list);
+        mv.setMv(hash);
         return mv;
     }
 }
